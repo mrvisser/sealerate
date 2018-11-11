b450fcd7ce0f89f17a807293e8aa97ad410a8da9
Philippus <philippus@gmail.com>
2018-11-11 14:06:39 +0100

    Remove unused dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,8 @@
 import sbt._
 
 object Dependencies {
-
   object Version {
-    val macros = "2.1.0"
     val scalaTest = "3.0.3"
-    val typesafeConfig  = "1.2.1"
-  }
-
-  object Compiler {
-    val macroParadise = "org.scalamacros" % "paradise" % Version.macros cross CrossVersion.full
-  }
-
-  object Compile {
-    val quasiQuotes = "org.scalamacros" %% "quasiquotes" % Version.macros cross CrossVersion.binary
   }
 
   object Test {
